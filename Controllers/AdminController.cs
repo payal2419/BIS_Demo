710cb896e3cb20aca767f47ea80595e1e48d076e
payal2419 <payal.s.pawar@gmail.com>
2021-09-07 20:38:09 +0530

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIS_Demo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterAuthor()
+        {
+            return View();
+        }
+
+
+        // Manage Books
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        public IActionResult SearchBook()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateBook()
+        {
+            return View();
+        }
+
+        // Table Display
+
+        public IActionResult TableDisplay()
+        {
+            return View();
+        }
+    }
+}
